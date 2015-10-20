20b2c31f1e3c34ff9d0678f7f688fe6fbdd5f2a6
Benjamin Eberlei <kontakt@beberlei.de>
2015-10-20 13:40:25 +0200

    Refactored TaskService

--- src/AppBundle/Service/TaskService.php
+++ src/AppBundle/Service/TaskService.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Service;
 
 use Doctrine\ORM\EntityManager;
+use DateTime;
 
 class TaskService
 {
@@ -19,16 +20,32 @@ class TaskService
 
     public function nextWeekReport(DateTime $start, Datetime $end, $includeHours = false)
     {
+        $tasks = $this->getNextWeeksTasks($start, $end);
+        $nextTasks = $this->getNextTasks($tasks, 3);
+        $chart = $this->generateChartFromTasks($tasks, $includeHours);
+        $this->sendTaskChart($chart, $tasks, $start, $end);
+    }
+
+    public function getNextWeeksTasks(DateTime $start, Datetime $end)
+    {
         $dql = 'SELECT t FROM AppBundle\Entity\Task t
                  WHERE t.dueDate >= ?1 AND t.dueDate <= ?2
               ORDER BY t.dueDate ASC';
-        $tasks = $this->entityManager->createQuery($dql)
+
+        return $this->entityManager->createQuery($dql)
             ->setParameter(1, $start)
             ->setParameter(2, $end)
             ->getResult();
+    }
 
+    public function getNextTasks(array $tasks, $amount = 3)
+    {
+        return array_slice($tasks, 0, $amount);
+    }
+
+    public function generateChartFromTasks(array $tasks, $includeHours)
+    {
         $chart = [];
-        $nextThreeTasks = array_slice($tasks, 0, 3);
         $format = $includeHours ? 'Y-m-d H' : 'Y-m-d';
 
         foreach ($tasks as $task) {
@@ -38,9 +55,14 @@ class TaskService
             $chart[$task->dueDate->format($format)]++;
         }
 
+        return $chart;
+    }
+
+    public function sendTaskChart(array $chart, array $nextTasks, $start, $end)
+    {
         $body = $this->templating->render(
             'Emails/dueDateChart.html.twig',
-            array('chart' => $chart, 'nextThreeTasks' => $nextThreeTasks)
+            array('chart' => $chart, 'nextThreeTasks' => $nextTasks)
         );
 
         $message = \Swift_Message::newInstance()
--- src/AppBundle/Tests/Integration/ServiceTest.php
+++ src/AppBundle/Tests/Integration/ServiceTest.php
@@ -2,16 +2,61 @@
 
 namespace AppBundle\Tests\Integration;
 
-use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use AppBundle\Service\TaskService;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\AbstractQuery;
+use Symfony\Component\Templating\EngineInterface;
+use DateTime;
+use AppBundle\Entity\Task;
 
-class ServiceTest extends KernelTestCase
+class ServiceTest extends \PHPUnit_Framework_TestCase
 {
-    public function testGet()
+    public function testTaskServicePhake()
     {
-        $kernel = self::createKernel();
-        $kernel->boot();
-        $container = $kernel->getcontainer();
+        $entityManager = \Phake::mock(EntityManager::class);
+        $query = \Phake::mock(AbstractQuery::class);
+        $templating = \Phake::mock(EngineInterface::class);
+        $mailer = \Phake::mock(\Swift_Transport::class);
 
-        $service = $container->get('task_service');
+        $service = new TaskService($entityManager, $mailer, $templating);
+
+        \Phake::when($entityManager)->createQuery(\Phake::anyParameters())->thenReturn($query);
+        \Phake::when($query)->setParameter(\Phake::anyParameters())->thenReturn($query);
+        \Phake::when($query)->getResult()->thenReturn(array());
+        \Phake::when($templating)->render(\Phake::anyParameters())->thenReturn('foo');
+
+        $service->nextWeekReport(new DateTime, new DateTime);
+
+        \Phake::verify($mailer)->send($this->isInstanceOf(\Swift_Message::class));
+    }
+
+    public function testTaskService()
+    {
+        $entityMock = $this->getMockBuilder('Doctrine\ORM\EntityManager')->setMethods(
+            ['createQuery', 'setParameter', 'getResult']
+        )->disableOriginalConstructor()->getMock();
+
+        $entityMock->method('createQuery')->willReturnSelf();
+        $entityMock->method('setParameter')->willReturnSelf();
+        $entityMock->method('getResult')->willReturn([$this->getTask(), $this->getTask(), $this->getTask()]);
+
+        $mailerMock = $this->getMockBuilder('SwiftMailer')->setMethods(['send'])->getMock();
+        $mailerMock->expects($this->once())->method('send')->with($this->attributeEqualTo('_body', ''));
+
+        $templatingMock = $this->getMockBuilder('Templating')->setMethods(['render'])->getMock();
+        $templatingMock->method('render')->willReturn('');
+
+        $service = new TaskService($entityMock, $mailerMock, $templatingMock);
+
+        $service->nextWeekReport(new DateTime(), new DateTime());
+    }
+
+    protected function getTask()
+    {
+        $task = new Task();
+        $task->id = rand(1, 1000);
+        $task->dueDate = new DateTime('2015-01-'.rand(1, 31));
+
+        return $task;
     }
 }
